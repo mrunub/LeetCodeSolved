acb90692da54a66625e1205405b6d2854ee11901
Mrunmayee Babhulkar <mrunub@gmail.com>
2021-02-01 23:07:04 +0530

    Update 001.Two Sum.py

--- 001.Two Sum.py
+++ 001.Two Sum.py
@@ -17,3 +17,17 @@ Details
 Runtime: 44 ms, faster than 85.48% of Python3 online submissions for Two Sum.
 Memory Usage: 14.4 MB, less than 48.63% of Python3 online submissions for Two Sum.
 '''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j] 
+                    break
+'''
+Success
+Details 
+Runtime: 36 ms, faster than 99.11% of Python3 online submissions for Two Sum.
+Memory Usage: 14.3 MB, less than 91.56% of Python3 online submissions for Two Sum.
+''''
